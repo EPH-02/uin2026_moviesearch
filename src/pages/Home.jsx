f96a64901416821019692e785f97f95457535d6b
EPH-02 <eskilhaugen@live.no>
2026-02-27 13:48:57 +0100

    endret på parametere, lagt til fetch og får opp data fra spørring

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,24 @@
 
 export default function Home(){
+    const baseUrl = `http://www.omdbapi.com/?t=james-bond&apikey=`
+    const apiKey = '8f4ddb22'
+
+    const getMovies = async()=>{
+        try{
+            const response = await fetch(`${baseUrl}${apiKey}`)
+            const data = await response.json()
+            console.log(data)
+        }
+        catch(err){
+            console.error(err);
+        }
+    }
+    
     return(
-        <h1>Forside</h1>
+        <main>
+            <h1>Forside</h1>
+            <button onClick={getMovies}>Hent filmer</button>
+        </main>
+        
     )
 }
